18d350d88135593bbab3aa1ce62b55e26eb207b6
sobuj <abdulaualsobuj@gmail.com>
2023-12-11 11:33:52 +0600

    plantshop-application

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Plantshop</a>
+      </header>
+      <main classname="">
+        list-product
+      </main>
+    </div>
+  );
+}
+
+export default App;
